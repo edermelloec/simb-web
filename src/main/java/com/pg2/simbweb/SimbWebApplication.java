533f3ab942075929af081448d7dc0b67f29393b5
Éder Mello <edermelloec@gmail.com>
2018-09-28 23:21:50 -0300

    teste

--- src/main/java/com/pg2/simbweb/SimbWebApplication.java
+++ src/main/java/com/pg2/simbweb/SimbWebApplication.java
@@ -1,5 +1,9 @@
 package com.pg2.simbweb;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,18 @@ public class SimbWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimbWebApplication.class, args);
+		
+		
+		try {
+			String s="postgres";
+			MessageDigest m;
+			m = MessageDigest.getInstance("MD5");
+			m.update(s.getBytes(),0,s.length());
+		       System.out.println("MD5: "+new BigInteger(1,m.digest()).toString(16));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	       
 	}
 }
